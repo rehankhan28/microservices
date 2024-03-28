428d8e10553cfdfa63698b5ec9175babe3829c31
Rehan <rehankhan.88c@gmail.com>
2024-03-28 20:18:07 +0530

    first commit

---
+++ Micro-Customer/src/main/java/com/micro/customer/service/ICustomerService.java
@@ -0,0 +1,13 @@
+package com.micro.customer.service;
+
+import com.micro.customer.model.CustomerDto;
+import com.micro.customer.model.LoginDTO;
+import com.micro.customer.model.RegisterDTO;
+
+public interface ICustomerService {
+	boolean registerCustomer(RegisterDTO registerDto);
+
+	boolean loginCustomer(LoginDTO loginDto);
+
+	CustomerDto readCustomer(Long phoneNumber);
+}
